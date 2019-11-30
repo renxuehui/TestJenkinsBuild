88083a6cd12a05c3b60b36026ef9c4c3d075447b
renxuehui-PC\renxuehui <renxuehui1983@163.com>
2019-11-30 11:07:05 +0800

    fixed: 测试分支合并冲突

--- TestBuildWeb/src/main/java/com/havedreams/jenkins/TestBuild.java
+++ TestBuildWeb/src/main/java/com/havedreams/jenkins/TestBuild.java
@@ -1,5 +1,20 @@
 package com.havedreams.jenkins;
 
-public class TestBuild {
+import java.io.IOException;
 
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.CompareResults;
+import org.gitlab4j.api.models.Diff;
+
+public class TestBuild {
+    private static GitLabApi gitLabApi = new GitLabApi("https://github.com/renxuehui/havedreams.git", "13560af0782eb2c5fb6278b2527f90ca95675616");
+    
+    public static void main(String[] args) throws IOException, GitLabApiException {
+        CompareResults compareResults = gitLabApi.getRepositoryApi().compare(123, "master", "release");
+        
+        for (Diff diff : compareResults.getDiffs()) {
+            diff.getDiff().indexOf("");
+        }
+    }
 }
